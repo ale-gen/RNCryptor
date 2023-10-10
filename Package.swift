91d5d472bc4578c9842255bdf4e754ffbc1cad5d
Aleksandra Generowicz <62150130+ale-gen@users.noreply.github.com>
2023-10-10 08:52:19 +0200

    Define multiple SPM products for different linking types

--- Package.swift
+++ Package.swift
@@ -9,7 +9,14 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RNCryptor",
+            targets: ["RNCryptor"]),
+        .library(
+            name: "RNCryptor-Dynamic",
             type: .dynamic,
+            targets: ["RNCryptor"]),
+        .library(
+            name: "RNCryptor-Static",
+            type: .static,
             targets: ["RNCryptor"]),
     ],
     targets: [
